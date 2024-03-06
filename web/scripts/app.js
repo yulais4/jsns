7abfce6764df214be034e9f00a84ff5944f611ce
gitea <gitea>
2024-03-06 15:38:58 +0300

    Added keyboard.

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -2,6 +2,69 @@ const appContents = document.querySelector(".app-contents");
 let isAppInit = false;
 appContents.style.display = "block";
 
+// Функция для обработки нажатия клавиши
+function handleKeyPress(event) {
+  var keyPressed = String.fromCharCode(event.which || event.keyCode);
+  
+  // Проверяем, что нажата буква
+  if (/^[a-zA-Z]$/.test(keyPressed)) {
+      console.log('Была нажата буква: ' + keyPressed);
+      // Здесь можно добавить свою логику для обработки нажатия определённой буквы
+      switch(keyPressed){
+        case 'Z':
+          osc1_start(261.63);
+          break;
+        case 'S':
+          osc1_start(277.18);
+          break;
+        case 'X':
+          osc1_start(293.66);
+          break;
+        case 'D':
+          osc1_start(311.13);
+          break;
+        case 'C': //E
+          osc1_start(329.63);
+          break;
+        case 'V': //F
+          osc1_start(343.23);
+          break;
+        case 'G': //F#
+          osc1_start(369.99);
+          break;
+        case 'B': //G
+          osc1_start(392);
+          break;
+        case 'H': //G#
+          osc1_start(415.3);
+          break;
+        case 'N': //A
+          osc1_start(440);
+          break;
+        case 'J': //A#
+          osc1_start(466.16);
+          break;
+        case 'M': //B
+          osc1_start(493.88);
+          break;
+      }
+  }
+}
+
+// Слушаем событие нажатия клавиши
+document.addEventListener('keydown', handleKeyPress);
+
+// Слушаем событие отпускания клавиши
+document.addEventListener('keyup', function(event) {
+  var keyReleased = String.fromCharCode(event.which || event.keyCode);
+  
+  // Проверяем, что отпущена буква
+  if (/^[a-zA-Z]$/.test(keyReleased)) {
+      console.log('Была отпущена буква: ' + keyReleased);
+      // Здесь можно добавить свою логику для обработки отпускания определённой буквы
+      osc1_stop();
+  }
+});
 
 C_key = document.getElementById("C_key");
 C_key.addEventListener("mousedown", () => osc1_start(261.63));
